d6a5c36cceeab63e3409124ff2544a82b3e5b8de
IJvred <reichchannel11@gmail.com>
2025-08-20 19:43:04 -0600

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-use tauri_plugin_sql::TauriSql;
-
 fn main() {
   tauri::Builder::default()
-    .plugin(TauriSql::default())
-    .setup(|app| {
-      let _main = app.get_webview_window("main");
-      Ok(())
-
-    })
+    .plugin(tauri_plugin_sql::Builder::new().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
